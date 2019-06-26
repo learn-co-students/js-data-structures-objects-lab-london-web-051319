891b113c22e51e380bc74e37cc03883aaeb8600c
flexkat <catherinemaritz1@gmail.com>
2019-06-26 14:46:47 +0100

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  let newDriver = Object.assign(driver, {"key": value});
+  let newDriver = Object.assign(driver, {[key]: value});
   return newDriver
 }
